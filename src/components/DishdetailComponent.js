c1a105fa4dc36b92f86f4a23ff112a002226f2bb
Animesh Mohanty <35626679+animeshmohanty@users.noreply.github.com>
2020-06-24 16:47:45 +0530

    Client Server Communication and Fetch

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,8 +1,11 @@
 import React,{Component} from 'react';
-import {Card, CardImg,CardText,CardBody,CardTitle,Modal,ModalHeader,ModalBody,Breadcrumb,BreadcrumbItem,Button,Row,Col,Label} from 'reactstrap';
+import {Card, CardImg, CardText, CardBody, CardTitle, Modal, ModalHeader, ModalBody, Breadcrumb, BreadcrumbItem, 
+        Button,Row,Col,Label} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -26,7 +29,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment)
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment)
   }
 
   render() {
@@ -116,7 +119,7 @@ function RenderDish( {dish} ) {
             return(
                 <div className='col-12 col-md-5 m-1'>
                 <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImg top width="100%" src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                       <CardTitle>{dish.name}</CardTitle>
                       <CardText>{dish.description}</CardText>
@@ -132,7 +135,7 @@ function RenderDish( {dish} ) {
 
 }
 
-function RenderComments( {comments, addComment, dishId} ) {
+function RenderComments( {comments, postComment, dishId} ) {
     if (comments == null) {
         return (
             <div></div>
@@ -159,7 +162,7 @@ function RenderComments( {comments, addComment, dishId} ) {
             <ul className='list-unstyled'>
                 {reviews}
             </ul>
-            <CommentForm dishId={dishId} addComment={addComment}/>  
+            <CommentForm dishId={dishId} postComment={postComment}/>  
         </div>
     );
 }
@@ -205,7 +208,7 @@ function RenderComments( {comments, addComment, dishId} ) {
                     <RenderDish dish={props.dish} />
                     <RenderComments 
                     comments={props.comments}
-                    addComment={props.addComment}
+                    postComment={props.postComment}
                     dishId={props.dish.id}
                     />
                </div>
